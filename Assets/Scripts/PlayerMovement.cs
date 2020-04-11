92dcd4f7aaa3b3ba8dbc39ad241ef121ce1bd836
Rhys Brailsford <rhysbrailsford@gmail.com>
2020-04-11 20:51:26 +0930

    Added throw behaviour to player and holdable object. Also refactored all player states into 2 enums. Disabled input movement when player is picking up object. Added relative rotation to picked up object so it matches the rotation of the player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,15 +25,22 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 knockbackVec;
 
     private GameObject grabbedObj = null;
+    public bool isGrabbingObj = false;
 
     private GameObject pickedUpObj = null;
-    private bool pickingUp = false;
     private float curPickingUpDuration = 0;
     public float pickingUpDuration = 1;
+    private Vector3 offsetRotation;
     private Vector3 objSrcPosition;
     private Vector3 objDestPosition;
 
-    private bool holdingObj = false;
+    public float throwStrength = 10;
+    public float throwDur = 1;
+    private float curThrowDur = 0;
+    private Vector3 throwVec;
+
+    public InteractState interactState = InteractState.Idle;
+    public MovementState movState = MovementState.Idle;
 
     // Start is called before the first frame update
     void Start()
@@ -41,162 +48,6 @@ public class PlayerMovement : MonoBehaviour
         scaledSpeed = speed / 10;
         currentDirection = Direction.South;
         movementVec = new Vector3(0, 0, 0);
-
-    }
-
-    public void DisableInputMovement()
-    {
-        inputMoveable = false;
-    }
-    public void EnableInputMovement()
-    {
-        inputMoveable = true;
-    }
-    public bool InputMoveable()
-    {
-        return inputMoveable;
-    }
-
-    public void DisableMoveable()
-    {
-        moveable = false;
-    }
-    public void EnableMovement()
-    {
-        moveable = true;
-    }
-    public bool Moveable()
-    {
-        return moveable;
-    }
-
-    public void Knockback(Vector3 srcPosition)
-    {
-        DisableInputMovement();
-
-        // Calculate knockback vector
-        // A->B = B-A
-        Vector3 curPosition = transform.position;
-        Vector3 preNormalised = curPosition - srcPosition;
-        preNormalised.y = 0;
-        knockbackVec = Vector3.Normalize(preNormalised);
-        //Debug.Log("knockback vector:" + knockbackVec);
-
-        curKnockbackRemaining = knockbackDuration;
-    }
-
-    public void Grab(GameObject obj)
-    {
-        grabbedObj = obj;
-        DisableInputMovement();
-    }
-
-    public void Ungrab()
-    {
-        grabbedObj = null;
-        EnableInputMovement();
-    }
-
-    public void PickupObject(GameObject obj)
-    {
-        // Maybe add argument for picked up item
-        pickingUp = true;
-        curPickingUpDuration = 0;
-        DisableInputMovement();
-        pickedUpObj = obj;
-        objSrcPosition = pickedUpObj.transform.position;
-        objDestPosition = gameObject.transform.position;
-        objDestPosition.y += 4.5f;
-    }
-
-    private void PickingUp()
-    {
-        if (curPickingUpDuration > pickingUpDuration)
-        {
-            pickingUp = false;
-            holdingObj = true;
-            EnableInputMovement();
-            return;
-        }
-
-        pickedUpObj.transform.position = Vector3.Lerp(objSrcPosition, objDestPosition, curPickingUpDuration / pickingUpDuration);
-
-        curPickingUpDuration += Time.deltaTime;
-    }
-
-    private void HoldingObj()
-    {
-        Vector3 temp = pickedUpObj.transform.position;
-        temp.x = gameObject.transform.position.x;
-        temp.z = gameObject.transform.position.z;
-        pickedUpObj.transform.position = temp;
-        // :TODO:
-        // do something about rotation
-    }
-
-    Vector3 CalculateMovementVec()
-    {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputZ = Input.GetAxisRaw("Vertical");
-
-        Vector3 result = Vector3.Normalize(new Vector3(inputX, 0, inputZ));
-        return result;
-    }
-
-    Vector3 CalculateNewPosition(Vector3 movementVec, float scale)
-    {
-        float curX = transform.position.x;
-        float curZ = transform.position.z;
-
-        float newX = curX + (movementVec.x * scale) * Time.deltaTime; 
-        float newZ = curZ + (movementVec.z * scale) * Time.deltaTime;
-        Vector3 newpositionVec = new Vector3(newX, transform.position.y, newZ);
-
-        return newpositionVec;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateInputAge();
-
-        if (inputMoveable)
-        {
-            movementVec = CalculateMovementVec();
-            movementVec = collisionScript.CalculateAdjustedMovement(movementVec);
-
-            gameObject.transform.position = CalculateNewPosition(movementVec, scaledSpeed);
-            currentDirection = CalculateDirection();
-            RotateToCurrentDirection();
-        }
-
-        if (holdingObj)
-        {
-            HoldingObj();
-        }
-
-        if (pickingUp)
-        {
-            PickingUp();
-        }
-
-        else if (curKnockbackRemaining > 0)
-        {
-            movementVec = collisionScript.CalculateAdjustedMovement(knockbackVec);
-
-            // Apply some knockback movement
-            gameObject.transform.position = CalculateNewPosition(movementVec, knockbackStrength);
-
-            curKnockbackRemaining -= Time.deltaTime;
-        }
-        else
-        {
-            // :TODO:
-            // Maybe put in someway to prevent player from buffering inputs while being knocked back.
-            // At the moment, the instant the knockback animation is done, inputs are being read and immediately being translated into movement. 
-            // Could use a delay, or force player to give new inputs.
-            EnableInputMovement();
-        }
     }
 
     void UpdateInputAge()
@@ -255,7 +106,7 @@ public class PlayerMovement : MonoBehaviour
 
         int index = inputAge.ToList().IndexOf(maxAge);
 
-        switch(index)
+        switch (index)
         {
             case (0):
                 return Direction.North;
@@ -277,7 +128,7 @@ public class PlayerMovement : MonoBehaviour
         // East: 0,90,0
         // West: 0,-90,0
         Vector3 newRotation = new Vector3(0, 0, 0);
-        switch(currentDirection)
+        switch (currentDirection)
         {
             case (Direction.North):
                 newRotation = new Vector3(0, 0, 0);
@@ -299,6 +150,236 @@ public class PlayerMovement : MonoBehaviour
         gameObject.transform.rotation = Quaternion.Euler(newRotation);
     }
 
-        
+    Vector3 CalculateMovementVec()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputZ = Input.GetAxisRaw("Vertical");
 
+        Vector3 result = Vector3.Normalize(new Vector3(inputX, 0, inputZ));
+        return result;
+    }
+
+    Vector3 CalculateNewPosition(Vector3 movementVec, float scale)
+    {
+        float curX = transform.position.x;
+        float curZ = transform.position.z;
+
+        float newX = curX + (movementVec.x * scale) * Time.deltaTime;
+        float newZ = curZ + (movementVec.z * scale) * Time.deltaTime;
+        Vector3 newpositionVec = new Vector3(newX, transform.position.y, newZ);
+
+        return newpositionVec;
+    }
+
+    public void DisableInputMovement()
+    {
+        inputMoveable = false;
+    }
+    public void EnableInputMovement()
+    {
+        inputMoveable = true;
+    }
+    public bool InputMoveable()
+    {
+        return inputMoveable;
+    }
+
+    public void DisableMoveable()
+    {
+        moveable = false;
+    }
+    public void EnableMovement()
+    {
+        moveable = true;
+    }
+    public bool Moveable()
+    {
+        return moveable;
+    }
+
+    public void Knockback(Vector3 srcPosition)
+    {
+        DisableInputMovement();
+
+        // Calculate knockback vector
+        // A->B = B-A
+        Vector3 curPosition = transform.position;
+        Vector3 preNormalised = curPosition - srcPosition;
+        preNormalised.y = 0;
+        knockbackVec = Vector3.Normalize(preNormalised);
+
+        curKnockbackRemaining = knockbackDuration;
+
+        movState = MovementState.KnockedBack;
+
+    }
+
+    private void KnockbackUpdate()
+    {
+        if (curKnockbackRemaining > 0)
+        {
+            movementVec = collisionScript.CalculateAdjustedMovement(knockbackVec);
+
+            // Apply some knockback movement
+            gameObject.transform.position = CalculateNewPosition(movementVec, knockbackStrength);
+
+            curKnockbackRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            EnableInputMovement();
+            movState = MovementState.Idle;
+        }
+    }
+
+    public void Grab(GameObject obj)
+    {
+        grabbedObj = obj;
+        DisableInputMovement();
+
+        interactState = InteractState.Grabbing;
+    }
+
+    public void Ungrab()
+    {
+        grabbedObj = null;
+        EnableInputMovement();
+
+        interactState = InteractState.Idle;
+    }
+
+    public void PickupObject(GameObject obj)
+    {
+        // Maybe add argument for picked up item
+        curPickingUpDuration = 0;
+        DisableInputMovement();
+        pickedUpObj = obj;
+        objSrcPosition = pickedUpObj.transform.position;
+        objDestPosition = gameObject.transform.position;
+        objDestPosition.y += 4.5f;
+
+        // Used to rotate object relative to players rotation when picked up
+        offsetRotation = gameObject.transform.rotation.eulerAngles;
+
+        interactState = InteractState.PickingUp;
+    }
+
+    private void PickingUpObjUpdate()
+    {
+        // Animation finished, swap to "holdingObj" state
+        if (curPickingUpDuration > pickingUpDuration)
+        {
+            interactState = InteractState.Holding;
+            EnableInputMovement();
+            return;
+        }
+
+        pickedUpObj.transform.position = Vector3.Lerp(objSrcPosition, objDestPosition, curPickingUpDuration / pickingUpDuration);
+
+        curPickingUpDuration += Time.deltaTime;
+    }
+
+    private void HoldingObjUpdate()
+    {
+        Vector3 temp = pickedUpObj.transform.position;
+        temp.x = gameObject.transform.position.x;
+        temp.z = gameObject.transform.position.z;
+        pickedUpObj.transform.position = temp;
+
+        // Rotate object given initial offset based on player rotation
+        pickedUpObj.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles - offsetRotation);
+    }
+
+    public void ThrowObj()
+    {
+
+        DisableInputMovement();
+        curThrowDur = 0;
+
+        interactState = InteractState.Throwing;
+    }
+
+    private void ThrowObjUpdate()
+    {
+        if (curThrowDur > throwDur)
+        {
+            Vector3 dest;
+            // Calculate destination of thrown object
+            switch (currentDirection)
+            {
+                case (Direction.East):
+                    dest = new Vector3(1, 0, 0);
+                    break;
+                case (Direction.North):
+                    dest = new Vector3(0, 0, 1);
+                    break;
+                case (Direction.South):
+                    dest = new Vector3(0, 0, -1);
+                    break;
+                case (Direction.West):
+                    dest = new Vector3(-1, 0, 0);
+                    break;
+                default:
+                    dest = new Vector3(0, 0, 1);
+                    break;
+            }
+            dest = dest * throwStrength;
+            dest = pickedUpObj.transform.position + dest;
+            pickedUpObj.GetComponent<HoldableObj>().Throw(dest);
+
+            // :Care:
+            // Ungrab was not initially designed to handle thrown objects, this could have negative side effects
+            Ungrab();
+
+            interactState = InteractState.Idle;
+            return;
+        }
+        curThrowDur += Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInputAge();
+
+        // Handle movement first
+        if (inputMoveable)
+        {
+            movementVec = CalculateMovementVec();
+            movementVec = collisionScript.CalculateAdjustedMovement(movementVec);
+
+            gameObject.transform.position = CalculateNewPosition(movementVec, scaledSpeed);
+            currentDirection = CalculateDirection();
+            RotateToCurrentDirection();
+        }
+        if (movState == MovementState.KnockedBack)
+        {
+            KnockbackUpdate();
+        }
+
+        if (interactState == InteractState.Holding)
+        {
+            HoldingObjUpdate();
+        }
+
+        if (interactState == InteractState.PickingUp)
+        {
+            PickingUpObjUpdate();
+        }
+
+        if (interactState == InteractState.Throwing)
+        {
+            ThrowObjUpdate();
+        }
+
+        
+        else
+        {
+            // :TODO:
+            // Maybe put in someway to prevent player from buffering inputs while being knocked back.
+            // At the moment, the instant the knockback animation is done, inputs are being read and immediately being translated into movement. 
+            // Could use a delay, or force player to give new inputs.
+            //EnableInputMovement();
+        }
+    }
 }
